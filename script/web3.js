6ddeeec0e76105de576100f3d98180e090dcc8c4
sunker0115 <sunker0115@163.com>
2020-11-12 23:50:04 +0800

    fix

--- script/web3.js
+++ script/web3.js
@@ -3,10 +3,10 @@ var Web3 = require("web3");
 var web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/v3/30bf4bface004c04b0ee6fa05753adca'));
 
 var fs = require("fs");
-var data = fs.readFileSync("./artifacts/contracts/BeefswapV2Factory.sol/BeefswapV2Factory.json", "utf-8");
+var data = fs.readFileSync("./build/StakingRewardsFactory.json", "utf-8");
 
 //创建合约对象
-var contract = new web3.eth.Contract(JSON.parse(data).abi,'0x1594E54b85F4BAd743F6613bd1215A4b6a4178Be');
+var contract = new web3.eth.Contract(JSON.parse(data).abi,'0x77Dcfe8904d571CF05e4EedFa81C22c3cB57c444');
 
 //调用合约的方法
-contract.methods.INIT_CODE_PAIR_HASH().call().then(console.log);
+contract.methods.deploy('0x1Af77a32A95Dc886C84604869b4B8Adc8264427a',5000000).call().then(console.log);
